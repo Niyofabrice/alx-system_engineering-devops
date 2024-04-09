ad51ceef74f55042f94b62d7c41b5b48778550fb
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-04-09 12:57:59 -0700

    getting subreddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-    """ function that returns number of subscribers for a given subreddit """
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
     import requests
 
-    subreddit_info = requests.get("https://reddit.com/r/{}/about/.json".format(subreddit))
-    return subreddit_info.json().get("data").get("subscribers")
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
